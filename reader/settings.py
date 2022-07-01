a376c71004be1ee5007abde776a4381524c71b1d
Jack Moreno <jack12972@gmail.com>
2022-07-01 18:22:18 -0500

    manage user

--- reader/settings.py
+++ reader/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'file_app.apps.FileAppConfig'
+    'file_app.apps.FileAppConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 
 # Internationalization
